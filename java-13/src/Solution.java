06a991d1bf1c8abe8ce7e3b9f0bb1dd43d428ab8
LVRodrigues <lvrodriguesline@gmail.com>
2024-07-12 13:08:52 -0300

    Java Currency Formatter

---
+++ java-13/src/Solution.java
@@ -0,0 +1,33 @@
+import java.text.NumberFormat;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) throws Exception {
+        try (Scanner scanner = new Scanner(System.in)) {
+            double value = scanner.nextDouble();
+
+            // I consider this corret with Java 15
+            // NumberFormat us = NumberFormat.getCurrencyInstance(Locale.US);
+            // NumberFormat in = NumberFormat.getCurrencyInstance(new Locale("en", "IN"));
+            // NumberFormat ch = NumberFormat.getCurrencyInstance(Locale.CHINA);
+            // NumberFormat fr = NumberFormat.getCurrencyInstance(Locale.FRANCE);
+
+            // System.out.println(String.format("US: %s", us.format(value)));
+            // System.out.println(String.format("India: %s", in.format(value)));
+            // System.out.println(String.format("China: %s", ch.format(value)));
+            // System.out.println(String.format("France: %s", fr.format(value)));
+
+            // On Hackerrank using Java 8
+            NumberFormat us = NumberFormat.getCurrencyInstance(Locale.US);
+            NumberFormat in = NumberFormat.getCurrencyInstance(new Locale("en", "IN"));
+            NumberFormat ch = NumberFormat.getCurrencyInstance(Locale.CHINA);
+            NumberFormat fr = NumberFormat.getCurrencyInstance(Locale.FRANCE);
+
+            System.out.println(String.format("US: %s", us.format(value)));
+            System.out.println(String.format("India: %s", in.format(value).replaceAll(in.getCurrency().getSymbol(), "Rs.")));
+            System.out.println(String.format("China: %s", ch.format(value)));
+            System.out.println(String.format("France: %s", fr.format(value)));
+        }
+    }
+}
