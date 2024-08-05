08fbc66a1862d72e8f5a11fe2639a2ecb78ee9e6
LVRodrigues <lvrodriguesline@gmail.com>
2024-08-05 16:13:22 -0300

    Valid Username Regular Expression

---
+++ java-23/src/Solution.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Solution {
+
+    private static final Pattern pattern = Pattern.compile("^[A-Z,a-z][A-Z,a-z,0-9,_]{7,29}");
+
+    public static void main(String[] args) throws Exception {
+        try (Scanner scanner = new Scanner(System.in)) {
+            int counter = scanner.nextInt();
+            if (counter < 1 || counter > 100) {
+                throw new Exception();
+            }
+            scanner.nextLine();
+            for (;counter > 0; counter--) {
+                String username = scanner.nextLine();
+                Matcher matcher = pattern.matcher(username);
+                if (matcher.matches()) {
+                    System.out.println("Valid");
+                } else {
+                    System.out.println("Invalid");
+                }
+            }
+        }
+    }
+}
+Valid Username Regular Expression
